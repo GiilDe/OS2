039f63cdc8b6dd4aeba4f6bface0a7538855a7c0
gilad212387 <38991777+gilad212387@users.noreply.github.com>
2018-11-30 13:09:27 +0200

    wait4 does not delete now waiting processes from changeables

--- sched.c
+++ sched.c
@@ -175,7 +175,7 @@ pid_t get_min_changeable() {
 	list_t *pos;
 	list_for_each(pos, &current_changeable) {
 		task_t *curr = list_entry(pos, task_t, run_list);
-        if(curr->state != TASK_RUNNING){
+        if(curr->state == TASK_RUNNING){
             pid_t pid = curr->pid;
             if (min_pid == -1) {
                 min_pid = pid;
